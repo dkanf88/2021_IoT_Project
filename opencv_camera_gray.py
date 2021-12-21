65697f9c5e9ac39932113580a152d17626f9aa0b
dkanf88 <84665308+dkanf88@users.noreply.github.com>
2021-12-21 14:12:50 +0900

    Add files via upload

---
+++ opencv_camera_gray.py
@@ -0,0 +1,35 @@
+import cv2
+
+cap = cv2.VideoCapture(0) 
+
+if not cap.isOpened():
+    print('Camera open failed')
+    exit()
+#fourcc(four character code)
+#DIVX(avi), MP4V(mp4), X264(h264)
+fourcc = cv2.VideoWriter_fourcc(*'DIVX') #('D', 'I', 'V', 'X')
+#ret, frame = cap.read()
+#cv2.imshow('frame', frame)
+#cv2.imwrite('output.jpg', frame)
+#cv2.waitkey(0)
+
+
+out = cv2.VideoWriter('output.avi', fourcc, 30, (640, 480))
+while True:
+    ret, frame = cap.read()
+    grayimage = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    edgeimage = cv2.Canny(frame, 50, 100)
+    if not ret:
+        break
+
+    cv2.imshow('frame', frame)
+    cv2.imshow('gray', grayimage)
+    cv2.imshow('edge', edgeimage)
+    out.write(frame)
+   
+    if cv2.waitKey(10) == 13:
+        break
+
+cap.release()
+out.release() 
+cv2.destroyAllWindows()
